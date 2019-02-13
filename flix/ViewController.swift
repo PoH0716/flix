03142b47c46c3e10dbf5c63ed7d5ec03dce80db3
Po Hsiang Huang <phh003@ucsd.edu>
2019-02-12 19:12:57 -0800

    Initial Commit

---
+++ flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flix
+//
+//  Created by Po Hsiang Huang on 2/12/19.
+//  Copyright © 2019 Po Hsiang Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
