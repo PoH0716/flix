0c9a8de3ada07758802887558a53f4b92ed4a254
Po Hsiang Huang <phh003@ucsd.edu>
2019-02-22 19:22:34 -0800

    part 2

---
+++ flix/MovieGridViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridViewCell.swift
+//  flix
+//
+//  Created by Po Hsiang Huang on 2/22/19.
+//  Copyright © 2019 Po Hsiang Huang. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
